8ed0a18e46d4fe7f12277631f437e602be181aba
mkrib <mkribben@mac.com>
2023-12-04 18:14:39 +0100

    huiswerk klaar om in te leveren

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,31 @@ public class Main {
         Integer[] numeric = {1,2,3,4,5,6,7,8,9,0};
         String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
         boolean play = true;
-        String ongeldig = "ongeldige invoer";
+        String ongeldig = "Ongeldige invoer";
 
-        Scanner scanner = new Scanner(System.in);
         Translator translator = new Translator(numeric, alphabetic);
+        Scanner scanner = new Scanner(System.in);
 
-        while (play = true) {
-
+        while (play) {
+            System.out.println("Type 'x' om te stoppen");
+            System.out.println("Type 'v' om te vertalen");
+            String input = scanner.nextLine();
+//            System.out.println(scanner.nextLine());
+            if (input.equals("x")) {
+                play = false;
+            } else if (input.equals("v")) {
+                System.out.println("Type een cijfer in van 0 t/m 9");
+                int number = scanner.nextInt();
+                if (number < 10) {
+                    String result = translator.translate(number);
+                    System.out.println("De vertaling van " + number + " is " + result);
+                } else {
+                    System.out.println(ongeldig);
+                }
+            }
+            else {
+                System.out.println(ongeldig);
+            }
         }
-
-
-
-        // Hier mag je je code schrijven voor de hoofd-opdracht
-
-
     }
 }
